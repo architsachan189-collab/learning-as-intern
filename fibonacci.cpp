6fb11ef3bc9fa6d85124d316081295416b46d4b8
architsachan189 <architsachan189@gmail.com>
2026-06-30 16:33:07 +0530

    My first commit

---
+++ fibonacci.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int fibonnaci(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    } 
+    return fibonnaci(n-1) + fibonnaci(n-2) ;
+}
+int main(){
+    int n ;
+    cout << "enter number :"  ;
+    cin >> n ;
+    for (int i = 0 ; i < n ; i++ ){
+        cout << fibonnaci(i) << " " ;
+    }
+    return 0;
+}
+//eucledian 
